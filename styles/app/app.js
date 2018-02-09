d49c8140c5968588b0df5f03695b998d20d988b4
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-02-09 11:29:08 -0700

    push three

--- styles/app/app.js
+++ styles/app/app.js
@@ -1,1 +1,16 @@
 angular.module('myApp', ['ui.router'])
+
+angular.module('myApp').config(function($stateProvider, $urlRouterProvider){
+    $stateProvider.state('home', { // 53E 
+        url: '/',
+        template: 'Home state',
+        controller: 'question'
+
+    }).state('easy', { //53G
+        url: '/easy',
+        templateUrl: "app/views/easy.html",
+        controller: 'questEasy'
+    })
+
+    $urlRouterProvider.otherwise('/') //53f
+})
