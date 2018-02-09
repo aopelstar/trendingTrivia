a9d838aca094775ba66764b40014fb0a35073a9a
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-02-09 10:29:59 -0700

    second push

--- styles/app/Services/questSrvc.js
+++ styles/app/Services/questSrvc.js
@@ -1,4 +1,9 @@
 angular.module('myapp').service('questSrvc', function($http){
     
+    this.getQuestions = function(){
+        return $http.get('https://practiceapi.devmountain.com/').then(resp => {
+            return resp.data.results
+        })
+    }
 }
 )
--- styles/app/question.js
+++ styles/app/question.js
@@ -1,3 +1,9 @@
 angular.module('myApp').controller('question', function($scope, questSrvc){
-    
+
+
+    $scope.questions = questSrvc.questions;
+
+    questSrvc.getQuestions().then(questions=>{
+        $scope.questions = questions;
+    })
 })
